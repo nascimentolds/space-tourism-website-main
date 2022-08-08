beefa15a88ad65fce15bb8c4ac5c7339993989e9
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2022-08-08 17:45:22 -0300

    Moon data completed

---
+++ assets/js/destination.js
@@ -0,0 +1,16 @@
+const showData = (result) => {
+  document.querySelector("#name").innerHTML = result.name;
+  document.querySelector("#description").innerHTML = result.description;
+  document.querySelector("#distance").innerHTML = result.distance;
+  document.querySelector("#travel").innerHTML = result.travel;
+  document.querySelector("#img").setAttribute("src", result.images.png);
+};
+
+function destination(bt) {
+  fetch("../../data/data.json")
+    .then((response) => response.json())
+    .then((data) => showData(data.destinations[bt]))
+    .catch((err) => console.log(err));
+}
+
+destination(0);
